3745593679a23278c822fc81b33972441bedf2dc
BrijalBaruzotech <brijal.parmar@baruzotech.com>
2022-01-25 15:46:34 +0530

    Git Practice

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import axios from "axios";
+import React from "react";
+
+export default class App extends React.Component{
+
+  
+
+    state = {
+        products : [],
+    }
+
+   componentDidMount(){
+
+        axios.get('https://fakestoreapi.com/products')
+
+            .then(res => {
+
+                console.log(res);
+                this.setState({products : res.data});
+
+            });
+   } 
+
+   render(){
+
+
+    
+         const productdata = this.state.products
+        return <div>
+
+          <ul>  
+
+
+                {productdata.map(product =>
+                   <>
+                    <li>Id: {product.id}</li>
+                    <li>Title: {product.title}</li>
+                    <li>Category: {product.category}</li>
+                    <li>Description{product.description}</li>
+                    <li>Price: {product.price}</li>
+                   </>
+                   )}
+                 
+        </ul>
+        </div>
+   }
+}
---
+++ src/First.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+import React from "react";
+
+export default class App extends React.Component{
+
+  
+
+    state = {
+        products : [],
+    }
+
+   componentDidMount(){
+
+        axios.get('https://fakestoreapi.com/products')
+
+            .then(res => {
+
+                console.log(res);
+                this.setState({products : res.data});
+
+            });
+   } 
+
+   render(){
+
+
+    
+         const productdata = this.state.products
+         console.log(Object.entries(productdata))
+         const objValue = Object.entries(productdata)
+        return <ul>  
+
+{objValue}
+
+                {objValue.map(product =>
+                   <>
+                    <li>{product.keys} : {product.values}</li>
+                   </>
+                   )}
+                 
+        </ul>
+   }
+}
